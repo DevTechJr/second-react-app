e0e0c837edba550353cf20d07f9d15e8568b1855
DevTechJr <devtechjr@gmail.com>
2022-01-30 16:36:26 -0500

    started commits

---
+++ src/SampleComponent/SampleCompo.jsx
@@ -0,0 +1,28 @@
+import "./SampleCompo.css";
+import React from "react";
+import { render } from "react-dom";
+import { useState } from "react";
+
+const SampleCompo = () => {
+  const [age, setAge] = useState(14);
+  const changeAge = () => {
+    setAge(age + 1);
+    console.log(age);
+  };
+  const downAge = () => {
+    setAge(age - 1);
+    console.log(`Age Changed => ${age}`);
+  };
+  return (
+    <div className="sample-compo">
+      <h1>
+        Change Age State Hook Trial <span>Button</span>
+      </h1>
+      <p>Current Age : {age}</p>
+      <button onClick={changeAge}>Change Age</button>
+      <button onClick={downAge}>Down Age</button>
+    </div>
+  );
+};
+
+export default SampleCompo;
